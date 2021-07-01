8bbf418353b9a5012b3d23c536a2cad79a79a7b6
Martin Kim Dung-Pham <martinkim.pham@gmail.com>
2021-07-01 21:51:23 +0200

    Initial Commit

---
+++ Mothership/MothershipApp.swift
@@ -0,0 +1,17 @@
+//
+//  MothershipApp.swift
+//  Mothership
+//
+//  Created by Martin Kim Dung-Pham on 01.07.21.
+//
+
+import SwiftUI
+
+@main
+struct MothershipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
