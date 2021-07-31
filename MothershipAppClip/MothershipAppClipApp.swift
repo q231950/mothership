2126f668289219a850c7e12976544a4b4733d391
Martin Kim Dung-Pham <martinkim.pham@gmail.com>
2021-07-31 20:15:05 +0200

    Add App Clip

---
+++ MothershipAppClip/MothershipAppClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  MothershipAppClipApp.swift
+//  MothershipAppClip
+//
+//  Created by Martin Kim Dung-Pham on 31.07.21.
+//
+
+import SwiftUI
+
+@main
+struct MothershipAppClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
